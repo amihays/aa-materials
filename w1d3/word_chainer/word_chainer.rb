cd9e8294e7bdedcc349f6f6966282a8fdbbcb68c
Ami Hays <amihays2@gmail.com>
2015-12-04 17:44:24 -0800

    Add path builder and stop early to word chainer

--- w1d3/word_chainer/word_chainer.rb
+++ w1d3/word_chainer/word_chainer.rb
@@ -16,16 +16,17 @@ class WordChainer
   def run(source, target)
     @current_words = [source]
     @all_seen_words = { source => nil }
-    until @current_words.empty?
+    until @current_words.empty? || @all_seen_words[target]
       explore_current_words
     end
+    build_path(target).join(" => ")
   end
 
   def explore_current_words
     new_current_words = []
     @current_words.each do |current_word|
       adjacent_words(current_word).each do |adjacent_word|
-        unless @all_seen_words[adjacent_word]
+        unless @all_seen_words.include?(adjacent_word)
           @all_seen_words[adjacent_word] = current_word
           new_current_words.push(adjacent_word)
         end
@@ -34,7 +35,17 @@ class WordChainer
     new_current_words.each { |word| puts "#{word} => #{@all_seen_words[word]}" }
     @current_words = new_current_words
   end
+
+  def build_path(target)
+    path = []
+    current_word = target
+    until current_word.nil?
+      path.push(current_word)
+      current_word = @all_seen_words[current_word]
+    end
+    path
+  end
 end
 
 wc = WordChainer.new("dictionary.txt")
-puts wc.run("market", "helper")
+puts wc.run("market", "backer")
