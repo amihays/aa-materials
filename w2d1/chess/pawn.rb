c7938d3bdf2927b2bf3e5c59c8893381a61cbe05
Ami Hays <amihays2@gmail.com>
2015-12-08 17:38:11 -0800

    Add Pawn moves method

--- w2d1/chess/pawn.rb
+++ w2d1/chess/pawn.rb
@@ -1,2 +1,27 @@
 class Pawn < Piece
+  def initialize(position, board, color)
+    super
+    @first_move = true
+  end
+
+  def move_dir
+    color == :black ? 1 : -1
+  end
+
+  def moves
+    positions = []
+    new_pos = [position[0] + move_dir, position[1]]
+    if board.in_bounds?(new_pos) &&
+      (board[new_pos].nil? || board[new_pos].color != color)
+      positions << new_pos
+    end
+    if @first_move
+      new_pos = [position[0] + move_dir * 2, position[1]]
+      if board.in_bounds?(new_pos) &&
+        (board[new_pos].nil? || board[new_pos].color != color)
+        positions << new_pos
+      end
+    end
+    positions
+  end
 end
