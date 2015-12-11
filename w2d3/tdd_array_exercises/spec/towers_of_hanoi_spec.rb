131c91ac77fd0bc0d2fa00cbd105c06ec906df27
Ami Hays <amihays2@gmail.com>
2015-12-10 18:04:44 -0800

    Add Towers of Hanoi #initialize and #move specs

---
+++ w2d3/tdd_array_exercises/spec/towers_of_hanoi_spec.rb
@@ -0,0 +1,28 @@
+require "rspec"
+require "towers_of_hanoi"
+
+describe "Towers of Hanoi" do
+  subject(:game) { Game.new }
+
+  describe "#initialize" do
+    it "initializes with three piles" do
+      expect(game.stacks.length).to eq(3)
+    end
+
+    it "initializes with full stack of towers on pile 1" do
+      expect(game.stacks[0]).to eq([3, 2, 1])
+    end
+  end
+
+  describe "#move" do
+    it "moves a disc from a pile to an empty pile" do
+      game.move(0, 1)
+      expect(game.stacks).to eq([[3,2], [1], []])
+    end
+
+    it "raises an error when moving a disc onto a smaller disc" do
+      game.move(0, 1)
+      expect { game.move(0, 1) }.to raise_error("Cannot move larger disc onto smaller disc")
+    end
+  end
+end
