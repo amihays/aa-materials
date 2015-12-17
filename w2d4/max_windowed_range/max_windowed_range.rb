614cfac56ed73a260f5e8cd023a5ae7fb894d78c
Ami Hays <amihays2@gmail.com>
2015-12-16 18:01:49 -0800

    Add optimized max_window_range using a MaxMinStackQueue

--- w2d4/max_windowed_range/max_windowed_range.rb
+++ w2d4/max_windowed_range/max_windowed_range.rb
@@ -1,9 +1,23 @@
-def windowed_max_range(array, range) # O(n * m) time, O(m) space
-  max_range = nil
+require_relative "max_min_stack_queue"
+
+def max_windowed_range(array, range) # O(n * m) time, O(m) space
+  max_diff = nil
   array.each_index do |idx1|
     sub_array = array.slice(idx1...idx1 + range)
     diff = sub_array.max - sub_array.min
-    max_range = diff if max_range.nil? || max_range < diff
+    max_diff = diff if max_diff.nil? || max_diff < diff
   end
-  max_range
+  max_diff
+end
+
+def max_windowed_range2(array, range) # O(n) time, O(m) space
+  queue = MaxMinStackQueue.new
+  max_diff = nil
+  array.each_index do |idx1|
+    queue.enqueue(array[idx1])
+    queue.dequeue if queue.size > range
+    diff = queue.max - queue.min
+    max_diff = diff if max_diff.nil? || max_diff < diff
+  end
+  max_diff
 end
