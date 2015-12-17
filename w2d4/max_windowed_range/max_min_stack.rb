4a7609be4f996bdae6f63a5e665f4313fc394865
Ami Hays <amihays2@gmail.com>
2015-12-16 17:41:26 -0800

    Add MaxMinStack class

---
+++ w2d4/max_windowed_range/max_min_stack.rb
@@ -0,0 +1,42 @@
+class MaxMinStack
+  def initialize
+    @store = []
+  end
+
+  def peek
+    store.last[:value] if store.last
+  end
+
+  def push(el)
+    new_min = (min.nil? || el < min) ? el : min
+    new_max = (max.nil? || el > max) ? el : max
+    new_el = { value: el,
+               current_min: new_min,
+               current_max: new_max}
+    store.push(new_el)
+    nil
+  end
+
+  def pop
+    store.pop.value
+  end
+
+  def min
+    store.last[:current_min] if store.last
+  end
+
+  def max
+    store.last[:current_max] if store.last
+  end
+
+  def size
+    store.size
+  end
+
+  def empty?
+    store.empty?
+  end
+
+  private
+  attr_reader :store
+end
