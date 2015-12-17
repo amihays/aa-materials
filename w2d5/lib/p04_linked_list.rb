1ba414feedccbbb2d78d1f742e50784aa6b75a22
Ami Hays <amihays2@gmail.com>
2015-12-16 20:36:24 -0800

    Add LinkedList class

--- w2d5/lib/p04_linked_list.rb
+++ w2d5/lib/p04_linked_list.rb
@@ -11,6 +11,7 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
   attr_reader :head
 
   def initialize
@@ -23,31 +24,57 @@ class LinkedList
   end
 
   def first
+    head.next
   end
 
   def last
+    current_link = head.next
+    return unless current_link
+    current_link = current_link.next until current_link.next.nil?
+    current_link
   end
 
   def empty?
+    head.next.nil?
   end
 
   def get(key)
+    result_link = find { |link| link.key  == key }
+    result_link ? result_link.val : nil
   end
 
   def include?(key)
+    any? { |link| link.key == key }
   end
 
   def insert(key, val)
+    new_link = Link.new(key, val, nil)
+    last.nil? ? head.next = new_link : last.next = new_link
   end
 
   def remove(key)
+    prev_link = head
+    current_link = head.next
+    until current_link.nil?
+      if current_link.key == key
+        prev_link.next = current_link.next
+        return current_link
+      end
+      prev_link, current_link = current_link, current_link.next
+    end
+    false
   end
 
   def each
+    current_link = head.next
+    until current_link.nil?
+      yield(current_link)
+      current_link = current_link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
