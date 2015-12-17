6e724e942b9942854296b9bd1aec8423271974e6
Ami Hays <amihays2@gmail.com>
2015-12-16 19:14:25 -0800

    Add MaxIntSet and IntSet

---
+++ w2d5/lib/p01_int_set.rb
@@ -0,0 +1,92 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    validate!(num)
+    store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    store[num]
+  end
+
+  private
+  attr_reader :store
+
+  def is_valid?(num)
+    num >= 0 && num < @store.length
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num].push(num) unless include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num) if include?(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+  attr_reader :store
+
+  def [](num)
+    store[num % store.length]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
