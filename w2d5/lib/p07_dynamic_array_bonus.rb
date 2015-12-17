bf64bfddb64e6143b5cf9038e246dc3c46a711aa
Ami Hays <amihays2@gmail.com>
2015-12-17 15:38:53 -0800

    Add DynamicArray - still has one weird bug

--- w2d5/lib/p07_dynamic_array_bonus.rb
+++ w2d5/lib/p07_dynamic_array_bonus.rb
@@ -1,4 +1,5 @@
 class StaticArray
+  attr_reader :store
   def initialize(capacity)
     @store = Array.new(capacity)
   end
@@ -13,6 +14,10 @@ class StaticArray
     @store[i] = val
   end
 
+  def length
+    @store.length
+  end
+
   private
 
   def validate!(i)
@@ -21,6 +26,7 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
   attr_reader :count
 
   def initialize(capacity = 8)
@@ -29,9 +35,23 @@ class DynamicArray
   end
 
   def [](i)
+    if i < 0 && i.abs < capacity
+      @store[count + i]
+    elsif i.abs < capacity
+      @store[i]
+    end
   end
 
   def []=(i, val)
+    if i >= @count
+      (i - @count).times { push(nil) }
+      push(val)
+    elsif i < 0
+      return nil if i < -(@count)
+      @store[@count + i] = val
+    else
+      @store[i] = val
+    end
   end
 
   def capacity
@@ -39,27 +59,73 @@ class DynamicArray
   end
 
   def include?(val)
+    any? { |el| el == val }
   end
 
   def push(val)
+    # One spec won't pass:
+    # "DynamicArray#resize! should call #resize!
+    # when too many elements are inserted"
+    # count == 3 and capacity == 3 in that spec
+
+    # Resizing works in all other specs when push is called.
+
+    # Works (although spec still fails because
+    # looks for "#resize!" call) if I change "resize!"
+    # to "resizee!"
+    resize! if @count >= self.capacity
+    puts(@count, self.capacity) if val == 4
+    @store[count] = val
+    @count += 1
+    self
   end
 
   def unshift(val)
+    resize! if count >= capacity
+    i = count
+    until i <= 0
+      @store[i] = @store[i - 1]
+      i -= 1
+    end
+    @store[0] = val
   end
 
   def pop
+    return nil if count == 0
+    last_el = last
+    @store[count - 1] = nil
+    @count -= 1
+    last_el
   end
 
   def shift
+    return nil if count == 0
+    first_el = first
+    i = 0
+    while i < @store.length - 1
+      @store[i] = @store[i + 1]
+      i += 1
+    end
+    @store[i] = nil
+    @count -= 1
+    first_el
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[count - 1]
   end
 
   def each
+    i = 0
+    while i < count
+      yield(@store[i])
+      i += 1
+    end
+    self
   end
 
   def to_s
@@ -68,7 +134,9 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    return false unless count == other.count
+    each_with_index { |el, i| return false unless el == other[i] }
+    true
   end
 
   alias_method :<<, :push
@@ -77,5 +145,9 @@ class DynamicArray
   private
 
   def resize!
+    puts "resizing!"
+    new_store = StaticArray.new(capacity * 2)
+    each_with_index { |el, i| new_store[i] = el }
+    @store = new_store
   end
 end
