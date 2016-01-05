b1a169097139303d346a0931bd227d0b542a626d
Ami Hays <amihays2@gmail.com>
2016-01-05 14:02:47 -0800

    Add max stack

--- pairboarding/day8.rb
+++ pairboarding/day8.rb
@@ -3,3 +3,26 @@ def missing_num(array)
   total_sum = array.length * (array.length + 1) / 2
   total_sum - sum
 end
+
+class Stack
+  def initialize
+    @values = []
+  end
+
+  def max
+    @values.last.last unless @values.empty?
+  end
+
+  def push(el)
+    if @values.empty?
+      @values << [el, el]
+    else
+      new_max = [max, el].max
+      @values << [el, new_max]
+    end
+  end
+
+  def pop
+    @values.pop.first
+  end
+end
