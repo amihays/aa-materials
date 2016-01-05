c00aaf730406312d7e8acbcf40db0c872bed21ee
Ami Hays <amihays2@gmail.com>
2016-01-05 15:12:29 -0800

    Add max window range in O(n) time

--- pairboarding/day8.rb
+++ pairboarding/day8.rb
@@ -27,7 +27,6 @@ class Stack
   end
 end
 
-
 class StackQueue
   def initialize
     @on_stack = []
@@ -44,4 +43,80 @@ class StackQueue
     end
     @off_stack.pop
   end
+end
+
+class MaxMinStack
+  def initialize
+    @values = []
+  end
+
+  def min
+    @values.empty? ? nil : @values.last[2]
+  end
+
+  def max
+    @values.empty? ? nil : @values.last[1]
+  end
+
+  def push(el)
+    if @values.empty?
+      @values << [el, el, el]
+    else
+      new_min = [min, el].min
+      new_max = [max, el].max
+      @values << [el, new_max, new_min]
+    end
+  end
+
+  def pop
+    @values.empty? ? nil : @values.pop[0]
+  end
+
+  def length
+    @values.length
+  end
+end
+
+class MinMaxStackQueue
+  def initialize
+    @on_stack = MaxMinStack.new
+    @off_stack = MaxMinStack.new
+  end
+
+  def enqueue(value)
+    @on_stack.push(value)
+  end
+
+  def dequeue
+    refill_off_stack if @off_stack.length == 0
+    @off_stack.pop
+  end
+
+  def refill_off_stack
+    @off_stack.push(@on_stack.pop) until @on_stack.length == 0
+  end
+
+  def length
+    @on_stack.length + @off_stack.length
+  end
+
+  def min
+    [@on_stack.min, @off_stack.min].reject { |el| el.nil? }.min
+  end
+
+  def max
+    [@on_stack.max, @off_stack.max].reject { |el| el.nil? }.max
+  end
+end
+
+def max_windowed_range(array, w)
+  max_range = nil
+  window = MinMaxStackQueue.new
+  array.each do |el|
+    window.enqueue(el)
+    window.dequeue if window.length > w
+    range = window.max - window.min
+    max_range = range if max_range.nil? || range > max_range
+  end
+  max_range
 end
